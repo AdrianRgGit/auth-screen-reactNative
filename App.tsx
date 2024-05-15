21d61a8fd57e5af9e2010b47755100f6427d9046
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-05-15 11:24:29 +0200

    de momento se quda así

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
+import Constants from "expo-constants";
 
-export default function App() {
+import background from "./public/images/background.jpg";
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Holaa</Text>
       <StatusBar style="auto" />
+      <View>
+        <Image source={background} />
+      </View>
+
+      <View>
+        <Text>Holaa</Text>
+        <Text>Sign in to your account</Text>
+      </View>
+
+      <View>
+        <TextInput placeholder="email@email.es" />
+        <TextInput placeholder="********" />
+        <Text>Forgot your Password?</Text>
+        <Button title="SIGN IN"></Button>
+      </View>
+
+      <Text>Don't have an account? Create</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    height: 50,
+    resizeMode: "cover",
   },
 });
+
+export default App;
